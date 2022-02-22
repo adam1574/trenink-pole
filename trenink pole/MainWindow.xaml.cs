43320797dc54f7a68478b16f0c80546aa395a802
Sivera.Adam <sivera.adam@seznam.cz>
2022-02-22 09:32:17 +0100

    Přidejte soubory projektu.

---
+++ trenink pole/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace trenink_pole
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int[] pole = new int[100];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GenerujPole(pole, -999,1000);
+            TxtBox.Text = string.Join("; ",pole) ;
+        }
+
+
+        private void GenerujPole(int[] array, int min, int max)
+        {
+            Random random = new Random();
+            for(int i = 0; i < pole.Length; i++)
+            {
+                array[i] = random.Next(min, max);
+            }
+        }
+
+
+        private int[] GenerujPole(int count, int min, int max)
+        {
+            int[] array = new int[count];
+
+            Random random = new Random();
+            for (int i = 0; i < pole.Length; i++)
+            {
+                array[i] = random.Next(min, max);
+            }
+            return array;
+        }
+
+
+        private int soucetprvkuvpoli(int[] array)
+        {
+            int soucet = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                soucet = soucet + array[i];
+            }
+        }
+    }
+}
